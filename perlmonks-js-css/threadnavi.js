ae53f72e542a6b61e5472bb887cc97f99cd4ca0d
Tina Mueller <tinita@cpan.org>
2013-09-01 14:44:59 +0200

    month off by one

--- perlmonks-js-css/threadnavi.js
+++ perlmonks-js-css/threadnavi.js
@@ -1,16 +1,16 @@
 var months = {
-    "Jan": 1,
-    "Feb": 2,
-    "Mar": 3,
-    "Apr": 4,
-    "May": 5,
-    "Jun": 6,
-    "Jul": 7,
-    "Aug": 8,
-    "Sep": 9,
-    "Oct": 10,
-    "Nov": 11,
-    "Dec": 12
+    "Jan": 0,
+    "Feb": 1,
+    "Mar": 2,
+    "Apr": 3,
+    "May": 4,
+    "Jun": 5,
+    "Jul": 6,
+    "Aug": 7,
+    "Sep": 8,
+    "Oct": 90,
+    "Nov": 10,
+    "Dec": 11
 };
 
 var notes;
